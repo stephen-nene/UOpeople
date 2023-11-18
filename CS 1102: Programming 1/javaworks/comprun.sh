133c4d3c1a23997af7bc65fdee0643a6d621da63
stephen-nene <stevekid705@mail.com>
2023-11-18 09:43:17 +0300

    have a compiler and runner

---
+++ CS 1102: Programming 1/javaworks/comprun.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cowsaystyles=("apt" "bud-frogs" "bunny" "calvin" "cheese" "cock" "cower" "daemon" "default" "dragon" "dragon-and-cow" "duck" "elephant" "elephant-in-snake" "eyes" "flaming-sheep" "fox" "ghostbusters" "gnu" "hellokitty" "kangaroo" "kiss" "koala" "kosh" "luke-koala" "mech-and-cow" "milk" "moofasa" "moose" "pony" "pony-smaller" "ren" "sheep" "skeleton" "snowman" "stegosaurus" "stimpy" "suse" "three-eyes" "turkey" "turtle" "tux" "unipony" "unipony-smaller" "vader" "vader-koala" "www")
+
+random_cowsay_style=${cowsaystyles[$RANDOM % ${#cowsaystyles[@]}]}
+
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <filename>"
+    exit 1
+fi
+
+# Get the filename from the argument
+filename="$1"
+
+# Check if the file has a .java extension
+if [[ ! "$filename" =~ \.java$ ]]; then
+    cowsay -f $random_cowsay_style "Wrong file type. Please provide a .java file."
+    exit 1
+fi
+
+# Extract the class name without the .java extension
+class_name=$(basename "$filename" .java)
+
+# Compile the Java file
+javac "$filename"
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+    # Run the Java program
+    java "$class_name"
+else
+    cowsay -f $random_cowsay_style "Compilation failed. Please check your code."
+fi
---
+++ CS 1102: Programming 1/javaworks/intro/Quime.java
@@ -0,0 +1,10 @@
+import textio.TextIO;
+// package intro;
+
+
+public class Quime {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
